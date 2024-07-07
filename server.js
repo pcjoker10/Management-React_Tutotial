c2e6988db20c9e13759f7ae0a740c7a0c001ac20
Jeon Seoungho <pcjoker10@gmail.com>
2024-07-07 19:42:18 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
+const fs = require('fs');
 const express = require('express');
-const budyParser = require('body-parser');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,33 +7,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://picsum.photos/id/1/64/64',
-            'name': '전우치',
-            'birthday': '961222',
-            'gender': '남자',
-            'job': '대학생'
-        },
-        {
-            'id': 2,
-            'image': 'https://picsum.photos/id/2/64/64',
-            'name': '홍길동',
-            'birthday': '961225',
-            'gender': '남자',
-            'job': '대학생'
-        },
-        {
-            'id': 3,
-            'image': 'https://picsum.photos/id/3/64/64',
-            'name': '춘향이',
-            'birthday': '961223',
-            'gender': '여자',
-            'job': '연예인'
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            console.log('The solution is:', rows[0])
+            res.send(rows);
         }
-    ])
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
