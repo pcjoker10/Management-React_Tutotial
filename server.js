755b5cd7580e5566582597c173c3ccfe36aefe37
Jeon Seoungho <pcjoker10@gmail.com>
2024-07-07 16:02:14 +0900

    Add Get Customers REST API Module

--- server.js
+++ server.js
@@ -7,8 +7,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id': 1,
+            'image': 'https://picsum.photos/id/1/64/64',
+            'name': '전우치',
+            'birthday': '961222',
+            'gender': '남자',
+            'job': '대학생'
+        },
+        {
+            'id': 2,
+            'image': 'https://picsum.photos/id/2/64/64',
+            'name': '홍길동',
+            'birthday': '961225',
+            'gender': '남자',
+            'job': '대학생'
+        },
+        {
+            'id': 3,
+            'image': 'https://picsum.photos/id/3/64/64',
+            'name': '춘향이',
+            'birthday': '961223',
+            'gender': '여자',
+            'job': '연예인'
+        }
+    ])
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
